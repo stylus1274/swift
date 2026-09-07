0f96cf3a193fff5a22a85e185371e599c7c54622
stylus1274 <charlieb@velvetinkmedia.com>
2026-09-07 18:29:04 -0400

    Load three-column mega menu styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./mega-menu.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://swiftconstructionandpainting.com"),
