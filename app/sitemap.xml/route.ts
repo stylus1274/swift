ff788b8e0ef4e5d8c88f3d3eee5d4b1de212b0d5
stylus1274 <charlieb@velvetinkmedia.com>
2026-09-22 19:02:13 -0400

    Update sitemap index domain and lastmod

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,7 +1,7 @@
-const siteUrl = "https://swift-nine-wheat.vercel.app";
+const siteUrl = "https://swiftconstructionandpainting.com";
 
 export function GET() {
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <sitemap>\n    <loc>${siteUrl}/sitemap-pages.xml</loc>\n    <lastmod>2026-09-14</lastmod>\n  </sitemap>\n  <sitemap>\n    <loc>${siteUrl}/sitemap-blog.xml</loc>\n    <lastmod>2026-09-14</lastmod>\n  </sitemap>\n</sitemapindex>`;
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <sitemap>\n    <loc>${siteUrl}/sitemap-pages.xml</loc>\n    <lastmod>2026-09-22</lastmod>\n  </sitemap>\n  <sitemap>\n    <loc>${siteUrl}/sitemap-blog.xml</loc>\n    <lastmod>2026-09-22</lastmod>\n  </sitemap>\n</sitemapindex>`;
 
   return new Response(xml, {
     headers: { "Content-Type": "application/xml; charset=utf-8" },
