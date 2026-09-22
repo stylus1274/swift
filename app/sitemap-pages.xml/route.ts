dc425ec52018fed4a830ecac48462adeade2738c
stylus1274 <charlieb@velvetinkmedia.com>
2026-09-22 19:02:16 -0400

    Update page sitemap routes and production domain

--- app/sitemap-pages.xml/route.ts
+++ app/sitemap-pages.xml/route.ts
@@ -1,4 +1,4 @@
-const siteUrl = "https://swift-nine-wheat.vercel.app";
+const siteUrl = "https://swiftconstructionandpainting.com";
 
 const pages = [
   "",
@@ -14,6 +14,7 @@ const pages = [
   "/interior-painting",
   "/kitchen-remodeling",
   "/multi-family-construction",
+  "/new-home-construction",
   "/custom-home-building",
   "/pressure-washing",
   "/reviews",
@@ -30,6 +31,7 @@ const pages = [
   "/projects/custom-farmhouse-home-brooksville-fl",
   "/projects/new-construction-duplex-spring-hill-fl",
   "/residential-painting",
+  "/residential-remodeling",
   "/home-remodeling",
   "/services",
   "/windows-doors",
